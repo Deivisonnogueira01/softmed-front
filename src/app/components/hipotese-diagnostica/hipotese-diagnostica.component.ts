8dd9147648718ce7d5f66e1fa0da6897626d8226
Deivison Nogueira <tp.deivison.nogueira@totvspartners.com.br>
2023-12-09 14:54:38 -0300

    styles hipotese part 1

--- src/app/components/hipotese-diagnostica/hipotese-diagnostica.component.ts
+++ src/app/components/hipotese-diagnostica/hipotese-diagnostica.component.ts
@@ -13,11 +13,11 @@ import { CasoClinicoService } from 'src/app/services/caso-clinico.service';
 })
 export class HipoteseDiagnosticaComponent implements OnInit {
 
-  casoClinico: CasoClinico | null = null; // Inicializado como null ou com um objeto padrão, conforme necessário
-  hipoteseDiagnostica: Patologia = Patologia.AMIGDALITE_AGUDA; // Inicializado com uma hipótese padrão ou vazia
-
+  casoClinico: CasoClinico | null = null;
+  hipoteseDiagnostica: Patologia = Patologia.AMIGDALITE_AGUDA;
 
   opcoesDiagnostico: Patologia[] = [];
+  bloquearProximaEtapa = true; // Inicializado como verdadeiro
 
   constructor(
     public dialog: MatDialog,
@@ -41,54 +41,45 @@ export class HipoteseDiagnosticaComponent implements OnInit {
   }
 
   embaralharOpcoesDiagnostico(): void {
-    // Crie uma cópia do enum para não alterar a ordem original
     const patologias = [...Object.values(Patologia)];
-  
-    // Remova a patologia correta, se estiver presente
     const index = patologias.indexOf(this.casoClinico?.patologia);
     if (index !== -1) {
       patologias.splice(index, 1);
     }
-  
-    // Embaralhe a ordem usando o algoritmo de Fisher-Yates
+
     for (let i = patologias.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [patologias[i], patologias[j]] = [patologias[j], patologias[i]];
     }
-  
-    // Adicione a patologia correta à lista de opções em uma posição aleatória
+
     const posicaoCorreta = Math.floor(Math.random() * 5);
     patologias.splice(posicaoCorreta, 0, this.casoClinico?.patologia);
-  
-    // Selecione as primeiras 5 patologias (ou quantas desejar)
+
     this.opcoesDiagnostico = patologias.slice(0, 5);
   }
-  
-  
-
 
   avaliarHipoteseDiagnostica(): void {
     if (this.hipoteseDiagnostica === this.casoClinico?.patologia) {
       this.toast.success('Hipótese correta!');
+      this.bloquearProximaEtapa = false; // Desbloqueia o botão após avaliação correta
     } else {
       this.toast.error('Hipótese incorreta. Tente novamente.');
       this.router.navigate([`/casos-clinicos/view/${this.casoClinico.casoClinicoId}`]);
-
-    
     }
   }
 
-
-  // Método para navegar para a próxima etapa
+ 
   navegarProximaEtapa(): void {
-    // Aqui você pode implementar a lógica de navegação para a próxima etapa ou componente
-    // Exemplo: this.router.navigate(['proximo-componente', { casoClinico: JSON.stringify(this.casoClinico) }]);
+    if (!this.bloquearProximaEtapa) {
+   
+      
+    }
+  }
+
+  avaliar(avaliacao: string): void {
+    this.router.navigate(['/home']);
+    this.toast.info("Avaliação Finalizada")
   }
 }
-
-
-
-
-
 
 
